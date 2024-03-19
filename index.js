7f561f1997e833fd90bc8234409deae9329de37a
kna1703 <nguyen.kna@gmail.com>
2024-03-19 11:20:14 +0100

    exercice couleurs collegue

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+const collegues = ["Charlotte", "Anne-so", "Charlot", "Joris"];
+console.log(chalk.yellow("Charlotte"));
+console.log(chalk.green("Anne-so"));
+console.log(chalk.blue("Charlot"));
+console.log(chalk.magenta("Joris"));
